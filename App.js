f42737ca2850b56250e7df41bd18316cfdfc83b3
alice <alice.prados07@gmail.com>
2026-03-30 21:35:33 -0300

    versão final

--- App.js
+++ App.js
@@ -4,15 +4,51 @@ import React, { useState, useEffect} from 'react';
 import { SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const dadosSimulados = Array.from({ length: 50}, (_,i) => ({
+        id: i.toString(),
+        nome: `Sensor ${1 + i} - linha de montagem`,
+        valor: '25°C'
+      }));
+      setData(dadosSimulados);
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  const renderItem = ({item}) => (
+    <View style={styles.card}>
+      <Text style={styles.itemTitle}>{item.nome}</Text>
+      <Text>{item.valor}</Text>
+    </View>
+    );
   return (
     <SafeAreaProvider>
     <SafeAreaView style={styles.container}>
+
       <ScrollView style={styles.headerArea}>
         <Text style={styles.title}>Monitoramento IoT - Unidade São Carlos</Text>
         <Text>{`Status: Conectado a rede industrial
         Última atualização: Agora`}</Text>
       </ScrollView>
-      <StatusBar style="auto" />
+
+      {loading?(
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#0000ff"/>
+          <Text>Carregando...</Text>
+        </View>
+      ):(
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={sensor => sensor.id}
+          contentContainerStyle={styles.listPadding}
+        />        
+      )}
+    <StatusBar style="auto" />
     </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -32,5 +68,17 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: 'bold',
     color: '#2276e4'
-  }
+  },
+  card:{
+    backgroundColor: '#fff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    borderRadius: 8,
+    elevation: 3,
+  },
+  itemTitle: { fontSize: 16, fontWeight: 'bold' },
+  listPadding: { paddingBottom: 20 },
+  loadingContainer: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+
 });
